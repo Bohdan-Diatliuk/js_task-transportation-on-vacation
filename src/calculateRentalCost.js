917e2df2045cd37f37b03ab6c3f7ab158a494583
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-09-02 19:38:46 +0300

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,20 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  const dayCost = 40;
-  const longTime = 7;
-  const shortTime = 3;
-  const longTimePrice = 50;
-  const shortTimePrice = 20;
+  const baseDailyRate = 40;
+  const LONG_TERM_THRESHOLD = 7;
+  const MID_TERM_THRESHOLD = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_DISCOUNT = 20;
   let discount = 0;
 
-  if (days >= longTime) {
-    discount = longTimePrice;
-  } else if (days >= shortTime) {
-    discount = shortTimePrice;
+  if (days >= LONG_TERM_THRESHOLD) {
+    discount = LONG_TERM_DISCOUNT;
+  } else if (days >= MID_TERM_THRESHOLD) {
+    discount = MID_TERM_DISCOUNT;
   }
 
-  return days * dayCost - discount;
+  return days * baseDailyRate - discount;
 }
 
 module.exports = calculateRentalCost;
